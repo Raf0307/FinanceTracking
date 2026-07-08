637cffdbe364cc6b34453758e150889b5da51164
Raf0307 <raf.flores.r@gmail.com>
2026-07-07 21:00:08 -0500

    Finished the second auth API. Moving onto completing some more middle ware before finishing the rest of the the auth apis

--- financeTrack/server/controllers/authController.js
+++ financeTrack/server/controllers/authController.js
@@ -1,29 +1,64 @@
 const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
 const User = require('../models/User');
+const { where } = require('sequelize');
 
 module.exports = {
     register: async (req, res) => {
         try {
-           const {email, password} = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({
-            email,
-            password_hash: hashedPassword,
-        })
-        res.status(201).json({
-            message: "User Registered succesfully"
-        }); 
+            const { email, password } = req.body;
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create({
+                email,
+                password_hash: hashedPassword,
+            })
+            res.status(201).json({
+                message: "User Registered succesfully"
+            });
         }
-        catch(error){
+        catch (error) {
             res.status(500).json({
                 message: "Error registering user",
                 error: error.message
             })
         }
-        
+
     },
     login: async (req, res) => {
-        //TODO: Finish this endpoint tomorrow
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({ where: { email } });
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Invalid credentials'
+                });
+            }
+            const isMatch = await bcrypt.compare(password, user.password_hash);
+            if (isMatch) {
+                //return the JWT token here
+                const token = JWT.sign(
+                    { id: user.id },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '24h' }
+                )
+
+                res.status(200).json({
+                    token: token
+                });
+            }
+            else {
+                res.status(401).json({
+                    message: "Invalid Credentials"
+                });
+            }
+
+        }
+        catch (error) {
+            res.status(500).json({
+                message: "Error Logging in user",
+                error: error.message
+            })
+        }
     },
     logout: async (req, res) => {
 
